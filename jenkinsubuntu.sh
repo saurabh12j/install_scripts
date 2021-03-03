fcc238811334d674cebfd50470d1c471b0945c44
saurabh12j <78630820+saurabh12j@users.noreply.github.com>
2021-03-02 23:43:20 -0600

    Update jenkinsubuntu.sh

--- jenkinsubuntu.sh
+++ jenkinsubuntu.sh
@@ -1,8 +1,8 @@
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 echo deb http://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt install openjdk-8-jdk
-sudo apt-get install jenkins
+sudo apt-get -qy update
+sudo apt-get -qy upgrade
+sudo apt -qy install openjdk-8-jdk
+sudo apt-get -qy install jenkins
 sudo systemctl start jenkins
 sudo systemctl status jenkins
